ad0a4f80a78897d3595b0d716a2946f18ebfcedd
Maestre <carlosa-maestreg@unilibre.edu.co>
2025-08-07 23:57:45 -0500

    Update task_model.py

--- task_model.py
+++ task_model.py
@@ -1,13 +1,26 @@
+# task_model.py
+
 class TaskModel:
     def __init__(self, task_name):
         self.task_name = task_name
         self.is_completed = False
 
     def get_task_name(self):
-            return self.task_name
-    
+        return self.task_name
+
     def mark_as_complete(self):
-         self.is_completed = True
+        self.is_completed = True
+
+    def set_done(self):
+        self.is_completed = True
+
+    def delete_task(self):
+        self.task_name = None
+        self.is_completed = False
+
+    def remove_task(self):
+        self.task_name = None
+        self.is_completed = False
 
     def is_completed(self):
-         self.is_completed
+        return self.is_completed

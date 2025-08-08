e1f74b92501cad6eb2bc67000fa0295b50a497bf
MICHIO-SAI <jhonydav2004@gmail.com>
2025-08-07 21:55:41 -0500

    Actualización de main.py con eliminación de tarea

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from task_model import TaskModel
 def main():
     task = TaskModel ("Estudiar para el examen")
     print(f"Tarea creada: {task.get_task_name()}")
-
+    task.mark_as_complete()
+    print(f"Tarea completada: {task.is_completed}")
+    task.remove_task()
+    print(f"Tarea eliminada:{task.get_task_name()}")
+    
 if __name__ == "__main__":
     main()

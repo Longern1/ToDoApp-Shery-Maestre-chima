6463abc060a8810f1645adbf183e4aaf1199c936
MICHIO-SAI <jhonydav2004@gmail.com>
2025-08-07 22:19:52 -0500

    Reemplazar main.py con cambios para tarea eliminada

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+# main.py
 from task_model import TaskModel
 
 def main():
-    task = TaskModel ("Estudiar para el examen")
+    task = TaskModel("Estudiar para el examen")
     print(f"Tarea creada: {task.get_task_name()}")
-    task.mark_as_complete()
-    print(f"Tarea completada: {task.is_completed}")
+    task.set_done()
+    print(f"Tarea completada: {task.is_done()}")
     task.remove_task()
-    print(f"Tarea eliminada:{task.get_task_name()}")
-    
+    print(f"Tarea eliminada: {task.get_task_name()}")
+
 if __name__ == "__main__":
     main()

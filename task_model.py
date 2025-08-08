bce7d607bb42fa651cc3acbf57426033705c8c0a
shery00845 <sheria-orozcoj@unilibre.edu.co>
2025-08-07 23:37:53 -0500

    Update task_model.py

--- task_model.py
+++ task_model.py
@@ -1,5 +1,15 @@
 class TaskModel:
     def __init__(self, task_name):
         self.task_name = task_name
-        def get_task_name(self):
+        self.is_completed = False
+
+    def get_task_name(self):
             return self.task_name
+
+    def delete_task(self):
+         self.task_name = None
+         self.is_completed = False
+
+    def is_completed(self):
+         return self.is_completed
+    
